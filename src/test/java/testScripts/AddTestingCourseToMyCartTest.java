9c3fe878ac3e5b72c0332299be03088129318ed6
Vinkatesh <Vinkatesh@DESKTOP-EOV6DE2>
2023-05-26 19:09:02 +0530

    First commit

---
+++ src/test/java/testScripts/AddTestingCourseToMyCartTest.java
@@ -0,0 +1,32 @@
+package testScripts;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import genericLibraries.BaseClass;
+
+public class AddTestingCourseToMyCartTest extends BaseClass
+{ 
+	@Test
+	public void addSeleniumToMyCartTest ()
+	{
+		SoftAssert soft = new SoftAssert();
+		home.clickGearsTab();
+		home.clickSkillraryDemoApp();
+		web.handleChildBrowser();
+		
+		soft.assertTrue(demoApp.getLogoText().contains("ECommerce"));
+		
+		demoApp.selectCategory(web, 1);
+		soft.assertEquals(testing.getPageHeader(),"Testing");
+		
+		web.scrollToElement(testing.getSeleniumImage());
+		web.dargAndDrop(testing.getSeleniumImage(), testing.getMyCart());
+		
+		web.scrollToElement(testing.getFacebookIcon());
+		testing.clickFacebook();
+		
+		soft.assertAll();
+	}
+
+}
